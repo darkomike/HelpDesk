13d17285ba567c749b48fdebcdf198ea33c35073
felixgyabaa <gyabaafelix01@gmail.com>
2023-01-31 15:01:30 +0000

    single upload

--- lib/screens/admin/index.dart
+++ lib/screens/admin/index.dart
@@ -3,7 +3,7 @@ export '../index.dart';
 export 'login.dart';
 export 'ticket_detail.dart';
 export 'admin_home.dart';
-export 'add_user.dart';
+export 'single_upload.dart';
 export 'batch_upload.dart';
 export 'create_user.dart';
 export 'new_users.dart';
